af2ef9ded5d2bd49d0ae3a8d8472f3caa7fd9570
Sasha Vasylenko <svasilenko389@gmail.com>
2022-08-27 23:14:13 +0300

    initial init

---
+++ main.js
@@ -0,0 +1,36 @@
+const store = {
+    name: "ITking",
+    adress: {
+        country: "Ukraine",
+        city: "Kyiv",
+        street: "Khreshchatyk",
+        houseNumber: "1"
+    }
+};
+
+let clients = {};
+clients['Oleksandr Vasylenko'] = {
+    quantity: 16,
+    bonuses: 412
+};
+
+clients['Nataliia Kulybina'] = {
+    quantity: 9,
+    bonuses: 242
+};
+
+clients['Oleksii Medvediev'] = {
+    quantity: 6,
+    bonuses: 113
+};
+
+clients['Yuriy Kruchinin'] = {
+    quantity: 1,
+    bonuses: 20
+}
+
+clients['Mykola Prymachok'] = {
+    quantity: 3,
+    bonuses: 42
+};
+
